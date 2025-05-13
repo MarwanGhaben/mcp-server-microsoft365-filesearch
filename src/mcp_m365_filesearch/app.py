6c10c565c601560af45f14fd44cc657db809380a
Marwan AbouGhaben <marwan_mostafa@hotmail.com>
2025-05-13 15:14:34 -0400

    Update app.py

--- src/mcp_m365_filesearch/app.py
+++ src/mcp_m365_filesearch/app.py
@@ -1,6 +1,5 @@
+import uvicorn
 from server import mcp
-import logging
 
 if __name__ == "__main__":
-    logging.info("Running MCP via app.py...")
-    mcp.run(host="0.0.0.0", port=10000)
+    uvicorn.run(mcp.app, host="0.0.0.0", port=10000)
